65b43253f89d818ee50336eea51f90c948f8e77e
Sanskar Lamsal <lamsal9677@yahoo.com>
2021-06-12 22:52:40 -0700

    Base

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Weather/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Weather from './components/Weather.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Weather
   }
 }
 </script>
---
+++ src/components/Weather.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{api_key}}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      api_key: '48857dff2053cf97163d8cafc103cc64'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
